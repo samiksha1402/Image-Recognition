8403f9116112a21d975de2033c3936767d87ff3f
samiksha1402 <80887833+samiksha1402@users.noreply.github.com>
2022-03-02 12:25:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+Webcam.set({
+    width: 350, 
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera= document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot(){
+ Webcam.snap(function(data_uri){
+     document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"/>';
+     
+ });
+}
+
+console.log("ml5 version: ", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/6U74W62Cw/model.json", model_loaded);
+
+function model_loaded(){
+    console.log("model is loaded");
+}
+
+function check(){
+    img= document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result){
+   if (error){
+       console.error(error);
+   }
+   else {
+       console.log(result);
+       document.getElementById("object_name").innerHTML= result[0].label;
+       document.getElementById("object_accuracy").innerHTML= result[0].confidence.toFixed(2);
+   }
+}
